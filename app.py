64abc174dc8e7662397d52102ea25e6db150105b
aviraljain355 <pggamer004@gmail.com>
2026-03-29 20:37:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,6 @@ def index():
 
     return render_template("index.html", hash_result=hash_result)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
